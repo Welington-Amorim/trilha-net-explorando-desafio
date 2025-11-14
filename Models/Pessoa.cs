62c68f1781a3562777b2ec6429e9a28d5f5c4ee3
Welington <welingtomwat@gmail.com>
2025-11-13 21:14:10 -0300

    Update Pessoa.cs

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -1,21 +1,18 @@
-namespace DesafioProjetoHospedagem.Models;
-
-public class Pessoa
+namespace DesafioProjetoHospedagem.Models
 {
-    public Pessoa() { }
-
-    public Pessoa(string nome)
+    public class Pessoa
     {
-        Nome = nome;
-    }
+        public Pessoa() {}
 
-    public Pessoa(string nome, string sobrenome)
-    {
-        Nome = nome;
-        Sobrenome = sobrenome;
-    }
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
 
-    public string Nome { get; set; }
-    public string Sobrenome { get; set; }
-    public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+    }
 }
